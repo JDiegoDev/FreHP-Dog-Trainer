2e81357787e5739fbc5b345cd6d2ddce65d4ada3
JDiegoDev <diego.gimenez.dev@gmail.com>
2024-03-26 20:32:28 -0600

    setting internationalization

---
+++ src/assets/languageResource.js
@@ -0,0 +1,33 @@
+const resources = {
+  en: {
+    translation: {
+      Header: {
+        home: "Home",
+        aboutUs: "About Us",
+        services: "Services",
+        title: "FreHp Dog Training Academy",
+      },
+      Main: {
+        welcome: "Welcome to freHP",
+        dogTraining: "Dog Training",
+        academy: "Academy",
+      },
+    },
+  },
+  sp: {
+    translation: {
+      Header: {
+        home: "Inicio",
+        aboutUs: "Nosotros",
+        services: "Servicios",
+        title: "FreHp Dog Training Academy",
+      },
+      Main: {
+        welcome: "Bienvenido a freHP",
+        dogTraining: "Dog Training",
+        academy: "Academy",
+      },
+    },
+  },
+};
+export default resources;
